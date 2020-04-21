9b29fe56ac727ed831c5c0cb5efccd7c5f00d197
Hey-Feres <brunoferesvillela@icloud.com>
2020-04-21 07:27:17 -0300

    Update Frontend

--- app/assets/javascripts/components/gym.js
+++ app/assets/javascripts/components/gym.js
@@ -4,51 +4,138 @@ class Gym {
         this.cnpj = cnpj;
         this.razao_social = razao_social;
         this.gym_id = gym_id;
-        this.options = {
-        	animation_delay: 500
-        }
     }
+    // Item 1 ______________________________________________________
     componentLoaded(){
-    	$(".divider").not("#wrapperDados").hide();
+        // .  .  .  .  .  .  .  .  .  .  .  .
+        $(".divider").not("#wrapperDados").hide();
+        // .  .  .  .  .  .  .  .  .  .  .  .
+        $("#gym-nome-loader").hide();
+        $("#gym-nome-check").hide();
+        $("#gym-nome-error").hide();
+        // .  .  .  .  .  .  .  .  .  .  .  .
+        $("#gym-razao-social-loader").hide();
+        $("#gym-razao-social-check").hide();
+        $("#gym-razao-social-error").hide();
+        // .  .  .  .  .  .  .  .  .  .  .  .
+        $("#gym-cnpj-loader").hide();
+        $("#gym-cnpj-check").hide();
+        $("#gym-cnpj-error").hide();
     }
+    // Item 2 ______________________________________________________
     mudarNome(novo_nome){
-		let request = new Request();
+        $("#gym-nome-loader").show();
+        let request = new Request();
 		let data = { "gym": {"nome": novo_nome} }
 		let url = apiBaseUrl + "/gyms/" + this.gym_id
-		let successCallback = function(data){ console.log(data) }
-		let errorCallback = function(jqXHR, textStatus, msg){ console.log(msg) }
-		const response = request.put(data, url, successCallback , errorCallback)
+		let successCallback = function(data){ 
+            console.log(data);
+            $("#gym-nome-check").show(200);
+            $("#gym-nome-loader").hide();
+            setTimeout(function(){ $("#gym-nome-check").hide(200); }, 2000);
+        }
+		let errorCallback = function(jqXHR, textStatus, msg){ 
+            console.log(msg);
+            $("#gym-nome-error").show(200);
+            $("#gym-nome-loader").hide();
+            setTimeout(function(){ $("#gym-nome-error").hide(200); }, 2000);
+        }
+        const response = request.put(data, url, successCallback , errorCallback)
 		return response
     }
+    // Item 3 ______________________________________________________
     mudarRazaoSocial(nova_razao_social){
 		let request = new Request();
 		let data = { "gym": {"razao_social": nova_razao_social} }
 		let url = apiBaseUrl + "/gyms/" + this.gym_id
-		let successCallback = function(data){ console.log(data) }
-		let errorCallback = function(jqXHR, textStatus, msg){ console.log(msg) }
+		let successCallback = function(data){ 
+            console.log(data);
+            $("#gym-razao-social-check").show(200);
+            $("#gym-razao-social-loader").hide();
+            setTimeout(function(){ $("#gym-razao-social-check").hide(200); }, 2000);
+        }
+		let errorCallback = function(jqXHR, textStatus, msg){ 
+            console.log(msg);
+            $("#gym-razao-social-error").show(200);
+            $("#gym-nome-loader").hide();
+            setTimeout(function(){ $("#gym-razao-social-error").hide(200); }, 2000);
+        }
 		const response = request.put(data, url, successCallback , errorCallback)
 		return response
-    }    
+    }
+    // Item 4 ______________________________________________________
     mudarCnpj(novo_cnpj){
 		let request = new Request();
 		let data = { "gym": {"cnpj": novo_cnpj} }
 		let url = apiBaseUrl + "/gyms/" + this.gym_id
-		let successCallback = function(data){ console.log(data) }
-		let errorCallback = function(jqXHR, textStatus, msg){ console.log(msg) }
+		let successCallback = function(data){ 
+            console.log(data);
+            $("#gym-cnpj-check").show(200);
+            $("#gym-cnpj-loader").hide();
+            setTimeout(function(){ $("#gym-cnpj-check").hide(200); }, 2000);
+        }
+		let errorCallback = function(jqXHR, textStatus, msg){ 
+            console.log(msg);
+            $("#gym-cnpj-error").show(200);
+            $("#gym-cnpj-loader").hide();
+            setTimeout(function(){ $("#gym-cnpj-error").hide(200); }, 2000);
+        }
 		const response = request.put(data, url, successCallback , errorCallback)
 		return response
     }
+    // Item 5 ______________________________________________________
     exibirWrapperDados(){
-    	$("#wrapperDados").show(this.options.animation_delay);
-    	$(".divider").not("#wrapperDados").hide(this.options.animation_delay);
+    	$("#wrapperDados").show(500);
+    	$(".divider").not("#wrapperDados").hide(500);
     }
-
+    // Item 6 ______________________________________________________
 	exibirWrapperEndereco(){
-    	$("#wrapperEndereco").show(this.options.animation_delay);
-    	$(".divider").not("#wrapperEndereco").hide(this.options.animation_delay);
+    	$("#wrapperEndereco").show(500);
+    	$(".divider").not("#wrapperEndereco").hide(500);
 	}
+    // Item 7 ______________________________________________________
 	exibirWrapperImagens(){
-    	$("#wrapperImagens").show(this.options.animation_delay);
-    	$(".divider").not("#wrapperImagens").hide(this.options.animation_delay);
+    	$("#wrapperImagens").show(500);
+    	$(".divider").not("#wrapperImagens").hide(500);
 	}
 }
+
+// Documentacao
+// Item 1 ________________________________________________________________________________
+//     - Metodo componentLoaded() é chamado quando a pagina é carregada. 
+//     - Ele inicializa as divs, esconde as que nao devem aparecer ao carregar 
+//     a pagina.
+// Item 2 ________________________________________________________________________________
+//    - Metodo mudarNome() é chamado sempre que houver alteração no input com nome da gym.
+//    - Ele recebe o valor digitiado no input como parametro.
+//    - Faz o handle de toda as etapas da requisicao ao backend
+//    - Exibe o icone de load enquanto carrega a requisicao
+//    - Exibe o icone de sucesso(check) quando a requisicao é bem sucedida
+//    - Exibe o icone de erro quando a requisicao retorna erros
+// Item 3 ________________________________________________________________________________
+//    - Metodo mudarRazaoSocial() é chamado sempre que houver alteração no input com 
+//    razao social da gym.
+//    - Ele recebe o valor digitiado no input como parametro.
+//    - Faz o handle de toda as etapas da requisicao ao backend
+//    - Exibe o icone de load enquanto carrega a requisicao
+//    - Exibe o icone de sucesso(check) quando a requisicao é bem sucedida
+//    - Exibe o icone de erro quando a requisicao retorna erros
+// Item 4 ________________________________________________________________________________
+//    - Metodo mudarCnpj() é chamado sempre que houver alteração no input com cnpj da gym.
+//    - Ele recebe o valor digitiado no input como parametro.
+//    - Faz o handle de toda as etapas da requisicao ao backend
+//    - Exibe o icone de load enquanto carrega a requisicao
+//    - Exibe o icone de sucesso(check) quando a requisicao é bem sucedida
+//    - Exibe o icone de erro quando a requisicao retorna erros
+// Item 5 ________________________________________________________________________________
+//    - Metodo exibirWrapperDados() é chamado quando o botao de dados (da sidebar) é 
+//    selecionado.
+//    - Exibe o wrapper de dados e esconde todos os outros
+// Item 6 ________________________________________________________________________________
+//    - Metodo exibirWrapperEndereco() é chamado quando o botao de endereco (da sidebar) é 
+//    selecionado.
+//    - Exibe o wrapper de endereco e esconde todos os outros
+// Item 7 ________________________________________________________________________________
+//    - Metodo exibirWrapperImagens() é chamado quando o botao de imagens (da sidebar) é 
+//    selecionado.
+//    - Exibe o wrapper de imagens e esconde todos os outros 
--- app/controllers/ui/gyms_controller.rb
+++ app/controllers/ui/gyms_controller.rb
@@ -1,6 +1,7 @@
 class Ui::GymsController < ApplicationController
 	def index
 		@gym = current_user.gym
+		# Item 1
 		@sidebar_items = {
 			img:"http://t0.gstatic.com/images?q=tbn:ANd9GcTVtaf929Lh1GpXfg_j11fhYkgNezEpzCou_EQsxMn4vYboHCtNQuTAWO2-A7oi_RlB7P4qVZvZn77mJFGePD4",
 			title: "Gym",
@@ -10,10 +11,46 @@ class Ui::GymsController < ApplicationController
 				{ title: "Imagens", id: "imagens-toggler", url: "#", first_item: false, last_item: true }
 			]
 		}
+		# Item 2
+		@inputs_attributes = {
+			gym_nome: {
+				label: "Nome da Gym", 
+				loader_id: "gym-nome-loader", 
+				done_id: "gym-nome-check", 
+				error_id: "gym-nome-error", 
+				placeholder: "Nome da Gym", 
+				input_id: "gym-nome", 
+				value: @gym.nome
+			},
+			gym_razao_social: {
+				label: "Razão Social", 
+				loader_id: "gym-razao-social-loader", 
+				done_id: "gym-razao-social-check", 
+				error_id: "gym-razao-social-error", 
+				placeholder: "Razão Social", 
+				input_id: "razao-social", 
+				value: @gym.razao_social
+			},
+			gym_cnpj: {
+				label: "CNPJ", 
+				loader_id: "gym-cnpj-loader", 
+				done_id: "gym-cnpj-check", 
+				error_id: "gym-cnpj-error", 
+				placeholder: "CNPJ", 
+				input_id: "cnpj", 
+				value: @gym.cnpj
+			}
+		}
 	end
 end
 
 # Documentacao
-# - Os itens com first_item = true ou last_item = true contam 
-# com esses atributos para ajustes do hover no css do list wraper no compoent sidebar
-# - Muitos items podem nao contar com uma url pois alguns botoes serao apenas para hide e show no script
+# Item 1 ________________________________________________________________________________
+# 	- Os itens com first_item = true ou last_item = true contam 
+# 	com esses atributos para ajustes do hover no css do list wraper no compoent sidebar
+# 	- Muitos items podem nao contar com uma url pois alguns botoes serao apenas para 
+# 	hide e show no script
+# Item 2 ________________________________________________________________________________
+# 	- A variavel inputs_attributes é um hash de hashes com os valores dos inputs e 
+# 	respectivos icones da  view Gym
+#  	- Todo o processamento dos inputs é feito na classe gym.js

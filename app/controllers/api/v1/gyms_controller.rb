2d85adc769ac3e4ae3f053a8c0eb3ba1123110ce
Hey-Feres <brunoferesvillela@icloud.com>
2020-05-04 21:04:05 -0300

    Update

--- app/controllers/api/v1/gyms_controller.rb
+++ app/controllers/api/v1/gyms_controller.rb
@@ -47,7 +47,3 @@ class Api::V1::GymsController < ApplicationController
 	      params.require(:gym).permit(:nome,:razao_social,:cnpj,:rua,:cidade,:estado,:cep,:numero,:lat,:lng,:imagens,:delete_image,:add_image)
 	    end
 end
-
-
-# Parameters: {"authenticity_token"=>"Lw/Dk6DCqHISsQhotPu1RmPwduSoAIbT8VilWf6Z+p2PrXXOayDQwpXmBC9nT9QxRpDHqRCQM/NOR1/I/N/Etw==", "imagens"=>["eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBDUT09IiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--22f96ab07a00ff1e608580ea336766c86c30d231"], "commit"=>"Save ", "id"=>"1"}
-# Parameters: {"gym"=>{"add_image"=>"true", "imagens"=>"blob:http://localhost:3000/026c0f32-48e7-4482-8645-e158e90f8e7a"}, "id"=>"1"}

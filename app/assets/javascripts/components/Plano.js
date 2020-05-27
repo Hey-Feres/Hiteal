810a6351991df86698073db46eb8ab1bba774263
Hey-Feres <brunoferesvillela@icloud.com>
2020-05-27 00:19:50 -0300

    Update

--- app/assets/javascripts/components/Plano.js
+++ app/assets/javascripts/components/Plano.js
@@ -1,11 +1,26 @@
 class Plano {
     constructor(id, nome, email, gym_id) {
         this.id = id;
-        this.nome = nome;
-        this.email = email;
         this.gym_id = gym_id;
     }
     componentLoaded(){
+        $(".sidebar").addClass("animated fadeInDownBig")
+        $(".content").addClass("animated fadeInUpBig")		
+        // .  .  .  .  .  .  .  .  .  .  .  .
+		$("#wrapperAdesao").hide()
+		$("#wrapperAdicionar").hide()
 
+		$("#wrapperEditarPlano").hide();
+    }
+    showEditorForm(id){
+    	$("#wrapperEditarPlano").show();
+    }
+    hideEditorForm(){
+    	$("#wrapperEditarPlano").hide();	
+    }
+    toggleWrappers(event, whatToShow){
+        event.preventDefault();
+        $(whatToShow).show(300);
+        $(".divider").not(whatToShow).hide(300);
     }
 }

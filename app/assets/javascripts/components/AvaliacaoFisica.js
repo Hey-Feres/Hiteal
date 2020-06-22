cd51cb0adf2b9fe79ee17ae867a5fe064865596e
Hey-Feres <brunoferesvillela@icloud.com>
2020-06-22 11:07:16 -0300

    Update

--- app/assets/javascripts/components/AvaliacaoFisica.js
+++ app/assets/javascripts/components/AvaliacaoFisica.js
@@ -1,8 +1,11 @@
 class AvaliacaoFisica {
 	constructor(gym_id) {
 		this.gym_id = gym_id;
+		this.sexo = null;
 	}
-
+	componentLoaded(){
+		this.sexo = "masculino"
+	}
 	searchAlunos(params, page){
         $(".sidebar-list-wrapper").append("<p id='loader'>Loading ...</p>")
         let request = new Request()
@@ -28,5 +31,56 @@ class AvaliacaoFisica {
         }
         let response = request.post(data,url,successCallback,errorCallback)
         return response
+	}
+	calcularIndiceDeMassaCorporal(altura, peso){
+		if ($("#massa").val() != "" && $("#estatura").val() != "") {
+			let imc = (peso/((altura/100)*(altura/100))).toFixed(2)
+			if (imc < 18.5) {
+				$("#indice-massa-corporal").val(imc + " - Abaixo do Peso Ideal")
+				$("#indice-massa-corporal").css("color", "rgb(90,200,250)")
+			}else if (imc >= 18.5 && imc <= 24.9){
+				$("#indice-massa-corporal").val(imc + " - Normal")
+				$("#indice-massa-corporal").css("color", "rgb(52,199,89)")
+			}else if (imc >= 25.0 && imc <= 29.9){
+				$("#indice-massa-corporal").val(imc + " - Sobrepeso (I)")
+				$("#indice-massa-corporal").css("color", "rgb(255,149,0)")
+			}else if (imc >= 30.0 && imc <= 39.9){
+				$("#indice-massa-corporal").val(imc + " - Obesidade (II)")
+				$("#indice-massa-corporal").css("color", "rgb(255,59,48)")
+			}else{
+				$("#indice-massa-corporal").val(imc + " - Obesidade Grave (III)")
+				$("#indice-massa-corporal").css("color", "rgb(251,52,0)")
+			}
+		}
+	}
+	calcularRelacaoCinturaQuadril(cintura, quadril, sexo){
+		if ( $("#cintura").val() != "" && $("#quadril").val() != "" ) {
+			let rcq = (cintura/quadril).toFixed(2)
+			if (this.sexo == "masculino") {
+				if (rcq <= 0.95) {
+					$("#relacao-cintura-quadril").val(rcq + " - Ótimo")
+					$("#relacao-cintura-quadril").css("color", "rgb(52,199,89)")
+				}else if(rcq > 0.95 && rcq <= 1.00){
+					$("#relacao-cintura-quadril").val(rcq + " - Atenção")
+					$("#relacao-cintura-quadril").css("color", "rgb(255,204,10)")
+				}else{
+					$("#relacao-cintura-quadril").val(rcq + " - Ruim")
+					$("#relacao-cintura-quadril").css("color", "rgb(255,59,48)")
+				}
+			} else {
+				if (rcq <= 0.80) {
+					$("#relacao-cintura-quadril").val(rcq + " - Ótimo")
+					$("#relacao-cintura-quadril").css("color", "rgb(52,199,89)")
+				}else if(rcq > 0.81 && rcq <= 0.85){
+					$("#relacao-cintura-quadril").val(rcq + " - Atenção")
+					$("#relacao-cintura-quadril").css("color", "rgb(255,204,10)")
+				}else{
+					$("#relacao-cintura-quadril").val(rcq + " - Ruim")
+					$("#relacao-cintura-quadril").css("color", "rgb(255,59,48)")
+				}
+			}
+
+
+		}
 	}
 }

cf95c6ba05ab01db420741aeccbc217ea510144e
Hey-Feres <brunoferesvillela@icloud.com>
2020-06-29 23:48:32 -0300

    Criando Tela Assinaturas

--- app/controllers/ui/assinaturas_controller.rb
+++ app/controllers/ui/assinaturas_controller.rb
@@ -1,5 +1,12 @@
 class Ui::AssinaturasController < ApplicationController
 	def index
-		
+		@sidebar_items = {
+			img:"https://img.icons8.com/ios/50/555555/pullups.png",
+			title: "Assinatura",
+			list_items: [
+				{ title: "Assinatura", id: "assinatura-toggler", url: "#", first_item: true, last_item: false },
+				{ title: "Assinatura", id: "adicionar-toggler", url: "#", first_item: false, last_item: true },
+			]
+		}
 	end
 end
